d1a51e9616f1b716e7b979243ecd23b24a176062
Ivan APEDO <apedoivan@gmail.com>
2026-04-18 11:44:19 +0200

    feat(aumeriax): update main.go

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: ccawk '{ print ... }' [file]")
+	var fieldSeparator string
+	flag.StringVar(&fieldSeparator, "F", "\t", "Field separator")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Usage: ccawk [-F separator] '{ print ... }' [file]")
 		os.Exit(1)
 	}
 
-	program := os.Args[1]
+	program := args[0]
 	var input *os.File
 	var err error
 
-	if len(os.Args) > 2 {
-		input, err = os.Open(os.Args[2])
+	if len(args) > 1 {
+		input, err = os.Open(args[1])
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
 			os.Exit(1)
@@ -37,10 +43,14 @@ func main() {
 	}
 
 	scanner := bufio.NewScanner(input)
+	recordNumber := 0
 	for scanner.Scan() {
+		recordNumber++
 		line := scanner.Text()
-		fields := strings.Fields(line)
-		interpreter := NewInterpreter(line, fields)
+		fields := strings.FieldsFunc(line, func(r rune) bool {
+			return strings.ContainsRune(fieldSeparator, r)
+		})
+		interpreter := NewInterpreter(line, fields, recordNumber, fieldSeparator)
 		interpreter.Execute(action)
 	}
 
